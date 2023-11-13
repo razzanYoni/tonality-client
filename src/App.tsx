3d60b1d660e91b8d3883e7dec9c582da618e321a
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-13 23:09:37 +0700

    refactor: add songs router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import reactLogo from './assets/react.svg'
 import AlbumRouter from './router/AlbumRouter';
+import SongsRouter from './router/SongsRouter';
 import './App.css'
 
 function App() {
   return (
     <>
         <AlbumRouter />
+        <SongsRouter />
     </>
   )
 }
