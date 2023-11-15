19bc23eeb7729d0a0f7984906fc46b25b46c13da
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-16 00:04:58 +0700

    refactor: album layout

--- src/layouts/AlbumLayout.tsx
+++ src/layouts/AlbumLayout.tsx
@@ -1,6 +1,10 @@
 // import React, { ReactNode } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from '../components/sidebar';
 import AlbumPage from '../pages/AlbumPage';
+import EditAlbum from '../pages/EditAlbumPage';
+import AddAlbum from '../pages/AddAlbumPage';
+import DeleteAlbumDialog from '@/components/delete-dialog-album';
 
 // interface AlbumLayoutProps {
 //   children: ReactNode;
@@ -8,12 +12,19 @@ import AlbumPage from '../pages/AlbumPage';
 
 const AlbumLayout= () => {
   return (
-    <div className="flex">
-      <Sidebar></Sidebar>
-      <div className='pl-20 pr-10'>
-       <AlbumPage></AlbumPage>
+    <BrowserRouter>
+      <div className="flex">
+        <Sidebar></Sidebar>
+        <div className='ml-20 pr-10'>
+          <Routes>
+            <Route path="/albums" element={<AlbumPage></AlbumPage>} />
+            <Route path="/1/edit-album" element={<EditAlbum></EditAlbum>} />
+            <Route path="/add-album" element={<AddAlbum></AddAlbum>} />
+            <Route path="/1/delete-album" element={<DeleteAlbumDialog></DeleteAlbumDialog>} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
