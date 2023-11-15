ba63362ce1d4a2f57b4966ef44a23f6b6f08b4f3
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-16 00:05:36 +0700

    refactor: styling

--- src/components/songs-table.tsx
+++ src/components/songs-table.tsx
@@ -1,4 +1,5 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+import { SongDropdown } from "./songs-dropdown";
 
 interface TableSongsProps {
     data: Array<{ id: number; title: string; duration: number }>;
@@ -16,22 +17,24 @@ export function TableSongs( { data }: TableSongsProps ) {
       };
 
     return (
-        <Table className="text-white ml-20">
+        <Table className="text-white ml-20 w-[790px] border">
             <TableHeader>
-                <TableRow>
+                <TableRow className="hover:bg-transparent">
                 <TableHead className="w-[10px]">#</TableHead>
-                <TableHead className="w-[800px]">Title</TableHead>
-                <TableHead>Duration</TableHead>
-                <TableHead className="text-right"></TableHead>
+                <TableHead className="w-[700px]">Title</TableHead>
+                <TableHead className="w-[70px]">Duration</TableHead>
+                <TableHead className="w-[10px]"></TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody className="text-left">
                 {data.map((song, index) => (
-                    <TableRow key={song.id}>
+                    <TableRow key={song.id} className="hover:bg-transparent">
                         <TableCell>{index + 1}</TableCell>
                         <TableCell>{song.title}</TableCell>
                         <TableCell>{formatDuration(song.duration)}</TableCell>
-                        <TableCell className="text-right"></TableCell>
+                        <TableCell className="">
+                            <SongDropdown/>
+                        </TableCell>
                     </TableRow>
             ))}
         </TableBody>
