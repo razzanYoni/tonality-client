60f35aade929713f8c62b817c975a59056633796
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-16 00:02:34 +0700

    refactor: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import reactLogo from './assets/react.svg'
-import AlbumRouter from './router/AlbumRouter';
-import SongsRouter from './router/SongsRouter';
+// import AlbumRouter from './router/AlbumRouter';
+// import SongsRouter from './router/SongsRouter';
+import AlbumLayout from './layouts/AlbumLayout';
+import SongsLayout from './layouts/SongsLayout';
 import './App.css'
 
 function App() {
   return (
     <>
-        <AlbumRouter />
-        <SongsRouter />
+        <AlbumLayout />
+        <SongsLayout />
     </>
   )
 }
