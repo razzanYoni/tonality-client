2c8d4a86e95d0d1024d5917c19d03f526a0fcd90
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-11-16 00:04:32 +0700

    feat: album delete

---
+++ src/components/delete-dialog-album.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+const DeleteAlbumDialog: React.FC = () => {
+
+
+  return (
+    <>
+      {(
+        <div className="fixed inset-0 overflow-y-auto z-50">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <div className="bg-white rounded p-8 z-10">
+              <div className="mb-4 text-lg font-semibold">Delete Album Confirmation</div>
+              <div className="mb-4">Are you sure you want to delete this album?</div>
+              <div className="flex justify-end">
+                <button
+                  className="px-4 py-2 mr-2 bg-gray-300 hover:bg-gray-400 rounded"
+                >
+                  Batal
+                </button>
+                <button
+                  className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded"
+                >
+                  Hapus
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DeleteAlbumDialog;
