2e074facfc61482df00cc85602765f9a679ad048
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-11-17 01:58:20 +0700

    resolve merge conflict

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg'
+// import AlbumRouter from './router/AlbumRouter';
+// import SongsRouter from './router/SongsRouter';
+// import AlbumLayout from './layouts/AlbumLayout';
+// import SongsLayout from './layouts/SongsLayout';
+import TonalityRouter from "./router/TonalityRouter.tsx";
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <TonalityRouter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/AlbumLayout.tsx
@@ -0,0 +1,31 @@
+// import React, { ReactNode } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from '../components/sidebar';
+import AlbumPage from '../pages/AlbumPage';
+import EditAlbum from '../pages/EditAlbumPage';
+import AddAlbum from '../pages/AddAlbumPage';
+// import DeleteAlbumDialog from '@/components/delete-dialog-album';
+
+// interface AlbumLayoutProps {
+//   children: ReactNode;
+// }
+
+const AlbumLayout= () => {
+  return (
+    <BrowserRouter>
+      <div className="flex">
+        <Sidebar></Sidebar>
+        <div className='ml-20 pr-10'>
+          <Routes>
+            <Route path="/albums" element={<AlbumPage></AlbumPage>} />
+            <Route path="/1/edit-album" element={<EditAlbum></EditAlbum>} />
+            <Route path="/add-album" element={<AddAlbum></AddAlbum>} />
+            {/*<Route path="/1/delete-album" element={<DeleteAlbumDialog></DeleteAlbumDialog>} />*/}
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AlbumLayout;
