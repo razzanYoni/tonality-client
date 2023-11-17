c3e07d792abf6e81e09ba6205e87e33a4ef7c8ee
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-11-17 11:48:44 +0700

    fix: TS error in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 import flattenDeep from "lodash.flattendeep";
- 
+import React from "react";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const generateFlattenRoutes = (routes) => {
-  if (!routes) return [];
-  return flattenDeep(routes.map(({ routes: subRoutes, ...rest }) => [rest, generateFlattenRoutes(subRoutes)]));
-};
+interface Route {
+  name: string;
+  title: string;
+  hasSiderLink?: boolean;
+  routes?: Route[];
+  component?: React.ComponentType<any>;
+  path?: string;
+  isPublic?: boolean;
+}
 
+export const generateFlattenRoutes = (routes: Route[] | undefined): Route[] => {
+  if (!routes) return [];
+  return flattenDeep(
+    routes.map(({ routes: subRoutes, ...rest }) => [
+      rest,
+      generateFlattenRoutes(subRoutes),
+    ]),
+  );
+};
